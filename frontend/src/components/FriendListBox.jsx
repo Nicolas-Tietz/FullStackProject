221bb7ac9a774aa7311bf370cc407cdfebdce5ac
Nicolas Tietz <nicolastietz48@gmail.com>
2024-01-11 22:08:14 +0100

    Reduced size of icon

--- frontend/src/components/FriendListBox.jsx
+++ frontend/src/components/FriendListBox.jsx
@@ -49,7 +49,7 @@ const FriendListBox = ({ friend }) => {
                 </div>
               )}
               <span className="absolute bottom-0 right-0 inline-flex items-center justify-center w-6 h-6 bg-blue-600 rounded-full">
-                <div className="h-full w-full">
+                <div className="h-4/5 w-4/5">
                   <FriendsIcon />
                 </div>
               </span>
